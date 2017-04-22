2cfdb3157814e0a7df5ef9658e92e3d9a52ad502
suraj <kolhesuraj@gmail.com>
2017-04-22 15:41:01 +0530

    task2

--- calculator.php
+++ calculator.php
@@ -1,17 +1,20 @@
 <?php
-	function sum($var1, $var2)
+	function sum($arr_input)
 	{
-		if($var1 == null || $var2 == null) {
-			$var1 = (($var1 == null) ? 0 : $var1);
-			$var2 = (($var2 == null) ? 0 : $var2);
+		if(!empty($arr_input)) {
+			$res = 0;
+			$arr_size = count($arr_input);
+			while($arr_size) {
+				$res += $arr_input[$arr_size-1];
+				$arr_size--;
+			}
+			echo $res."\n";
+		} else {
+			echo "0\n";
 		}
-
-		$res = $var1 + $var2;
-		echo $res."\n";
 	}
 
-	$var1 = (isset($argv[2]) ? $argv[2] : null);
-	$var2 = (isset($argv[3]) ? $argv[3] : null);
+	$arr_input = array_slice($argv, 2);
 
-	echo sum($var1, $var2);
+	echo sum($arr_input);
 ?>
